2a0083c383eff7ac305ff71ff462fdecf92a40c5
Lea Delic <leadelic0429@gmail.com>
2024-11-03 17:30:53 -0500

    Implement async/await to fetch ticket data and handle errors.

---
+++ main.js
@@ -0,0 +1,16 @@
+//task 2
+
+async function getData(Unresolved.Tickets) {
+    try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${Unresolved.Tickets}`);
+        
+        if (!response.ok) {
+            throw new Error('Not found');
+        }
+
+        const product = await response.json();
+        console.log(`Unresolved Ticket: ${unresolved.tickets}found`);
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
